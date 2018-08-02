407b14d258441b7243babbae7309f061c3e69e74
Mahmoud Gamie <mimg414@gmail.com>
2018-08-03 01:15:05 +0200

    styles: adjusted contrast ratio for colors

--- sw.js
+++ sw.js
@@ -25,31 +25,31 @@ self.addEventListener('install', function (event) {
     );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
     event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
-          // if no cache available clone the request 
-          var fetchRequest = event.request.clone();
-          return fetch(fetchRequest).then(
-            function(response) {
-              // Check if we received a valid response
-              if(!response || response.status !== 200 || response.type !== 'basic') {
-                return response;
-              }
-              //after cloning the request and the response we put it in the cashe?
-              var responseToCache = response.clone();  
-              caches.open('restaurant-service-worker')
-                .then(function(cache) {
-                  cache.put(event.request, responseToCache);
-                });
-              return response;
-            }
-          );
-        })
-      );
-  });
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                // if no cache available clone the request 
+                var fetchRequest = event.request.clone();
+                return fetch(fetchRequest).then(
+                    function (response) {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        //after cloning the request and the response we put it in the cashe?
+                        var responseToCache = response.clone();
+                        caches.open('restaurant-service-worker')
+                            .then(function (cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+                        return response;
+                    }
+                );
+            })
+    );
+});
